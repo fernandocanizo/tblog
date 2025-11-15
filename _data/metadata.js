d6693d8b506e13e31f7885cbb24685a11efb9cf0
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-11-15 07:28:54 -0300

    Add my own data to metadata file

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Fernando L. Canizo's Technical Journey",
+	url: "https://tblog.github.io/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Just another software developer's unrequested stories.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Fernando Lucio Canizo",
+		email: "fernandocanizo+tblog@gmail.com",
+		url: "https://tblog.github.io/about-me/"
 	}
 }
